a46c215ff8b7740b4d1a0c8076f9270d0268eb06
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2023-11-30 09:56:39 -0800

    Implementado metodo en caso de querer cargar partida o no. Reajustado SplashScreen

--- src/GUI/Test.java
+++ src/GUI/Test.java
@@ -13,6 +13,7 @@ import java.io.*;
  */
 public class Test {
     public static void main(String[] args) {
+        boolean cargar = false;
         String nombre;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         File Archivo = new File("archivo.txt");
@@ -20,8 +21,25 @@ public class Test {
         ObjectInputStream object;
         Juego juegoCarga;
 
+        SplashScreen splash = new SplashScreen();
+        splash.setVisible(true);
+
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        splash.dispose();
 
         if(Archivo.exists()){
+            int opcion = JOptionPane.showConfirmDialog(null, "Tiene una partida guardada desea cargala?");
+            if(opcion == JOptionPane.YES_OPTION)
+                cargar = true;
+        }
+
+
+        if(cargar){
         try {
             ArchivoGuardado = new FileInputStream(Archivo);
         } catch (FileNotFoundException e) {
@@ -41,17 +59,6 @@ public class Test {
             java.awt.EventQueue.invokeLater(() -> new VentanaP((int) screenSize.getWidth(), (int) screenSize.getHeight(), juegoCarga));
         } else {
 
-        /*SplashScreen splash = new SplashScreen();
-        splash.setVisible(true);
-
-        try {
-            Thread.sleep(2500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        splash.dispose();
-        */
             nombre = JOptionPane.showInputDialog("Digite su nombre");
 
             java.awt.EventQueue.invokeLater(() -> new VentanaP((int) screenSize.getWidth(), (int) screenSize.getHeight(), nombre));
